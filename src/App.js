a9c09871a980453e11574cdb0c635c58df6c6915
graemetg <graemetg@gmail.com>
2020-11-16 17:13:10 +0000

    added title to page via react helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Helmet>
+        <title>Duddleswell Tea Rooms | Home</title>
+      </Helmet>
         <p>
           DUDDLESWELL TEA ROOMS
         </p>
